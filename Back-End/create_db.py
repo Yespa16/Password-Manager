624e7ee6f1b47462acb95c6ce525570400757005
Yespa16 <codingyespa@gmail.com>
2022-07-18 22:52:26 +0400

    Create database with 'user' table

---
+++ Back-End/create_db.py
@@ -0,0 +1,33 @@
+"""
+This file was created to show how the database was created.
+In this file there is also examples how to manipulate the database.
+"""
+
+import sqlite3 
+
+
+conn = sqlite3.connect("database.db")
+
+c = conn.cursor()
+
+
+c.execute("""CREATE TABLE user(
+        email VARCHAR(100) unique,
+        password VARCHAR(50),
+        pin INTEGER(4)
+        )""")
+
+
+conn.commit()
+## Create a user
+c.execute("INSERT INTO user VALUES('example1@gmail.com', 'password', '1234' )")
+conn.commit()
+
+## Select user from
+c.execute("SELECT * FROM user WHERE email='codingyespa@gmail.com' ")
+
+print(c.fetchone())
+
+
+
+conn.close()
